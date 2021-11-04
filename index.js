c2a79862777438b436fbeb6669997be6b92683b2
Chris <chris.h.felt@gmail.com>
2021-11-03 20:17:05 -0600

    stand up basic server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+const express = require('express');
+
+const app = express();
+
+// all accepts any HTTP method
+app.all('*', function (req, res) {
+    // express handles basic headers and mime-types automatically
+    res.send("<h1>Demo page</h1>");
+})
+
+app.listen(3000, function () {
+    console.log("Listening on port 3000. Go to http://localhost:3000");
+});
