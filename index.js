02453ce93c6afc49c0ab51896a68b68233940e5a
Chris <chris.h.felt@gmail.com>
2021-11-04 20:06:22 -0600

    add cars route

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 // all accepts any HTTP method
-app.all('*', function (req, res) {
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+//app.use(express.static(path.join(__dirname, 'static')))
+
+/*
+app.get('/', function (req, res) {    
+    res.sendFile(path.join(__dirname, 'static/index.html'))
+});
+*/
+
+/*app.all('*', function (req, res) {
     // express handles basic headers and mime-types automatically
     res.send("<h1>Demo page</h1>");
 })
+*/
+
+app.get('/index', (request, response) => {
+  response.render('index', {
+    subject: 'Pug template engine',
+    name: 'our template',
+    link: 'https://google.com'
+  });
+});
+
 
 app.listen(3000, function () {
     console.log("Listening on port 3000. Go to http://localhost:3000");
