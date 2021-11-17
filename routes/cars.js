70e9942e94b46baf1b24a50ff73cb139efcbffc0
Chris <chris.h.felt@gmail.com>
2021-11-16 17:51:03 -0700

    added to cars route and template

--- routes/cars.js
+++ routes/cars.js
@@ -2,11 +2,28 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req,res) => {
-  res.status(200).json({"data":
-    [{"name":"chris", "age":32},
+
+let items = [{"name":"chris", "age":32},
     {"name":"crosby", "age":0}]
-  })
+
+//res.status(200).json({"data":})
+
+router.get("/", (req,res) => {
+   res.render('cars', {
+     subject: 'Pug template engine',
+     name: 'cars',
+     items: 'car stuff'
+   })
 });
+
+/*
+app.get('/index', (request, response) => {
+  response.render('index', {
+    subject: 'Pug template engine',
+    name: 'our template',
+    link: 'https://google.com'
+  });
+});
+*/
 
 module.exports = router;
